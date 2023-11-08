8968521c243a8893b024cde9a745ebd4b49dee6d
Markus Iorio <mbalzert@gmail.com>
2023-11-08 16:24:46 +0100

    use .env

--- buberdinner/api/main.py
+++ buberdinner/api/main.py
@@ -1,11 +1,11 @@
 """Buber Dinner api."""
 import fastapi
 
-from buberdinner.api.properties import config
+from buberdinner.api.properties import settings
 from buberdinner.api.v1 import Authcontroller
 
 app = fastapi.FastAPI(
-    title=config.get("api", "title"),
-    openapi_url=f"/api/{config.get('api','version')}/openapi.json",
+    title=settings.get("title"),
+    openapi_url=f"/api/{settings.get('version')}/openapi.json",
 )
-app.include_router(Authcontroller, prefix=f"/{config.get('api', 'version')}")
+app.include_router(Authcontroller, prefix=f"/{settings.get('version')}")
--- buberdinner/api/properties/__init__.py
+++ buberdinner/api/properties/__init__.py
@@ -1,7 +1,21 @@
-import configparser
 import pathlib
 
-root = pathlib.Path(__file__).absolute().parent
+from starlette import config as starlette_config
+from starlette import datastructures
 
-config = configparser.ConfigParser()
-config.read(root / "appsettings.dev.ini")
+properties = pathlib.Path(__file__).absolute().parent
+
+env = starlette_config.Config(properties / ".env")
+
+api_title = env.get("TITLE", default="buberdinner")
+api_version = env.get("VERSION", default="v1")
+
+host = env.get("HOST", default="127.0.0.1")
+port = env.get("PORT", default=5432)
+user = env.get("USER", default="postgres")
+password = datastructures.Secret(env.get("PASSWORD"))
+database = env.get("DATABASE", default=api_title)
+
+dsn = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+settings = {"title": api_title, "version": api_version, "dsn": dsn}
