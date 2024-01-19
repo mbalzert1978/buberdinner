ddab5027902e748e35ec4c1fc70e82091d8a10db
Markus Iorio <mbalzert@gmail.com>
2024-01-20 00:16:41 +0100

    jwt settings

--- buberdinner/app/dependencies.py
+++ buberdinner/app/dependencies.py
@@ -1,7 +1,9 @@
+import functools
 import typing
 
 import fastapi
 
+from buberdinner.api.core.config import get_app_settings
 from buberdinner.app.common.interfaces.authentication import ITokenGenerator
 from buberdinner.app.common.interfaces.services import IProvider
 from buberdinner.app.services.authentication import (
@@ -9,6 +11,7 @@ from buberdinner.app.services.authentication import (
     IAuthentication,
 )
 from buberdinner.infrastructure.authentication import JwtTokenGenerator
+from buberdinner.infrastructure.authentication.jwt_settings import JwtSettings
 from buberdinner.infrastructure.services import DateTimeProvider
 
 
@@ -19,15 +22,28 @@ def get_dt_provider() -> IProvider:
 Provider = typing.Annotated[IProvider, fastapi.Depends(get_dt_provider)]
 
 
-def get_jwt(provider: Provider) -> ITokenGenerator:
-    return JwtTokenGenerator(provider)
+@functools.lru_cache
+def get_jwt_settings() -> JwtSettings:
+    settings = get_app_settings()
+    return JwtSettings(
+        issuer=settings.issuer,
+        expire_in_days=settings.jwt_token_expires_in_days,
+        secret_key=settings.secret_key,
+    )
+
+
+TokenSettings = typing.Annotated[JwtSettings, fastapi.Depends(get_jwt_settings)]
+
+
+def get_jwt(provider: Provider, token_settings: TokenSettings) -> ITokenGenerator:
+    return JwtTokenGenerator(provider, token_settings)
 
 
 TokenGenerator = typing.Annotated[ITokenGenerator, fastapi.Depends(get_jwt)]
 
 
-def authentication_service(jwt_gen: TokenGenerator) -> IAuthentication:
-    return AuthenticationService(jwt_generator=jwt_gen)
+def authentication_service(jwt_generator: TokenGenerator) -> IAuthentication:
+    return AuthenticationService(jwt_generator=jwt_generator)
 
 
 Authentication = typing.Annotated[
--- buberdinner/infrastructure/authentication/jwt_token_generator.py
+++ buberdinner/infrastructure/authentication/jwt_token_generator.py
@@ -2,26 +2,26 @@ import uuid
 
 import jwt
 
-from buberdinner.api.core.config import get_app_settings
 from buberdinner.app.common.interfaces.services import IProvider
+from buberdinner.infrastructure.authentication import JwtSettings
 
 
 class JwtTokenGenerator:
     ALGORITHM = "HS256"
 
-    def __init__(self, datetime_provider: IProvider) -> None:
-        self._secret_key = get_app_settings().secret_key
+    def __init__(self, datetime_provider: IProvider, settings: JwtSettings) -> None:
+        self._settings = settings
         self._datetime_provider = datetime_provider
 
     def generate_token(self, user_id: uuid.UUID, first_name: str, last_name) -> str:
         return jwt.encode(
             {
-                "issuer": "Buberdinner",
+                "issuer": self._settings.issuer,
                 "user_id": str(user_id),
                 "first_name": first_name,
                 "last_name": last_name,
-                "exp": self._datetime_provider.add_days(1),
+                "exp": self._datetime_provider.add_days(self._settings.expire_in_days),
             },
-            self._secret_key.get_secret_value(),
+            self._settings.secret_key.get_secret_value(),
             algorithm=self.ALGORITHM,
         )
