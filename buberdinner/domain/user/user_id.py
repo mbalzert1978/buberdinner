f2174fb60adabf560d9b6a7f9863a904ad0b79d7
Markus Iorio <mbalzert@gmail.com>
2024-02-10 23:36:01 +0100

    refactor

--- buberdinner/domain/user/user_id.py
+++ buberdinner/domain/user/user_id.py
@@ -2,20 +2,26 @@ import dataclasses
 import typing
 import uuid
 
+DETAIL = "Comparisation between %s and %s is not implemented."
+UUID_FN = uuid.uuid4
 
-@dataclasses.dataclass
+
+@dataclasses.dataclass(frozen=True)
 class UserId:
     value: uuid.UUID
 
     @classmethod
     def create(cls) -> typing.Self:
-        return cls(uuid.uuid4())
+        return cls(UUID_FN())
 
     def __eq__(self, __value: object) -> bool:
         cls = type(self)
         if not isinstance(__value, cls):
-            raise NotImplementedError()
+            raise NotImplementedError(DETAIL % (cls, type(__value)))
         return self.value == __value.value
+
+    def __hash__(self) -> int:
+        return hash(self.value)
 
     def __str__(self) -> str:
         return str(self.value)
