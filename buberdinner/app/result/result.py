89588c1b1d59945ac8921680c7ae939bcb58b9bc
Markus Iorio <mbalzert@gmail.com>
2024-02-16 12:17:21 +0100

    fix bug

--- buberdinner/app/result/result.py
+++ buberdinner/app/result/result.py
@@ -1,7 +1,8 @@
 import typing
 
+from result import Err, Ok
+
 from buberdinner.app.error.common.error import Error
-from buberdinner.app.result import Err, Ok
 
 T = typing.TypeVar("T")
 E = typing.TypeVar("E", bound=Error)
